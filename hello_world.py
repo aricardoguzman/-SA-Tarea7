982435098821ca1a45c90ffca91cdfcede4eb587
aricardoguzman <201010425@ingenieria.usac.edu.gt>
2020-04-16 23:29:34 -0600

    first commit

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def wrap_html(message):
+    html = """
+        <html>
+        <body>
+            <div style='font-size:120px;'>
+            <center>
+                <image height="200" width="800" src="https://www.akamai.com/es/es/multimedia/images/article/akamai-dev-ops-overview-image.jpg?imwidth=1366">
+                <br>
+                {0}<br>
+            </center>
+            </div>
+        </body>
+        </html>""".format(message)
+    return html
+
+@app.route('/')
+def hello_world():
+    message = 'Hello SA T7!'
+    html = wrap_html(message)
+    return html
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
